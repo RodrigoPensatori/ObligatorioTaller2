546236274fbbd9b2ba03cd84de92040a76423b00
RodrigoPensatori <rodrigo.pensatori@hotmail.com>
2022-08-01 09:03:30 -0300

    p

--- www/js/codigo.js
+++ www/js/codigo.js
@@ -473,14 +473,17 @@ async function confirm() {
 
 function MostrarMapaUsr()
 {
-	var map = L.map('mapa').setView([-34.90364050627812, -56.190527957423214], 13);
+	var map = L.map('mapa').setView([-34.90364050627812, -56.190527957423214], 7);
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.marker([51.5, -0.09]).addTo(map)
-		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+	
+
+	L.marker([-34.90364050627812, -56.190527957423214]).addTo(map)
+		.bindPopup('Pepe')
 		.openPopup();
 }
 
